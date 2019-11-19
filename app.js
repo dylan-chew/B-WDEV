557cde7b760bff8b4c9c249baef8eaad33976034
Dylan Chew <dylanw.chew@gmail.com>
2019-11-19 13:57:06 -0400

    setting up heroku

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var app = express();
 dotenv.config();
 
 //Establish connection to Mongo
-mongoose.connect('mongodb://localhost/film', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to Mongo..."))
   .catch(error => console.log(error));
 
